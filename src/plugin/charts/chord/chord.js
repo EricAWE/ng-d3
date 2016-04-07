ef9ad42c6f1de1baede6b48777a09756a4963a75
Eric Berlin <eberlin@awe.fr>
2016-04-07 11:04:50 +0200

    test transition

--- src/plugin/charts/chord/chord.js
+++ src/plugin/charts/chord/chord.js
@@ -103,7 +103,7 @@ module.exports = function(ngD3) {
 
             pvs.chord.matrix(pvs.matrix);
             legend.renderMatrix();
-            _transition(old);
+            self.render();
         }
     }
 
